7b8df2c247eb8aa063768c1d187b40727bc8a335
Barbara Ratto <babi.ratto@gmail.com>
2025-01-13 10:08:06 +0000

    ex5

--- assignment_2_part2/src/slalom.cpp
+++ assignment_2_part2/src/slalom.cpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+//#include "interfaces/srv/lin_choice.hpp"
 
 
 // --------------------------------------------------------------------------------------------------------		
@@ -17,6 +18,11 @@ class Subscriber : public rclcpp::Node
 		Subscriber() : Node("subscriber")
         {
                 subscription = this->create_subscription<nav_msgs::msg::Odometry>( "/odom", 10, std::bind(&Subscriber::odom_callback, this, _1));
+                
+                // Publishers
+        		publisher = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+        		feet_pub = this->create_publisher<geometry_msgs::msg::Point>("position_feet", 10);
+
 	}
 
 	private:
@@ -29,25 +35,32 @@ class Subscriber : public rclcpp::Node
                 RCLCPP_INFO(this->get_logger(), "\nPosition x: %f\nPosition y: %f\n\n", msg->pose.pose.position.x, msg->pose.pose.position.y);
 
             	// implementing the required behaviour
-		my_vel.linear.x = 1.0;
+		my_vel.linear.x = 1;
 		my_vel.angular.z = 0;
 	
 		// making the turtle moving as request
 		if (msg->pose.pose.position.x > 9.0)
 		{
-			my_vel.linear.x = 1.0;			// linear velocity
+			my_vel.linear.x = 1;			// linear velocity
 			my_vel.angular.z = 2.0;		// angular vel > 0--> to turn left
 		}
 	
 		if (msg->pose.pose.position.x < 2.0)
 		{
-			my_vel.linear.x = 1.0;			// linear velocity
+			my_vel.linear.x = 1;			// linear velocity
 			my_vel.angular.z = -2.0;		// angular vel < 0 --> to turn right
 		}
 
 
 
            	publisher->publish(my_vel);
+           	
+           	geometry_msgs::msg::Point pos;
+			
+		pos.x = msg->pose.pose.position.x * 3.28;
+		pos.y = msg->pose.pose.position.y * 3.28;
+			
+		publisher->publish(pos);
             
             
 	}
@@ -61,11 +74,7 @@ int main(int argc, char * argv[])
 {
 	// Initialize the node
       	rclcpp::init(argc, argv);
-      	auto node = rclcpp::Node::make_shared("node");
-      	// initializing a publisher
-     	publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
-	// ros spin
       	rclcpp::spin(std::make_shared<Subscriber>());
       	rclcpp::shutdown();
       	return 0;
